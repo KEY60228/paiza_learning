c1b6205f7bfa411bd60d83e1f794b072e2f73fe7
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-07-07 22:47:52 +0900

    c_rank_level_up/c_rank_std_in_out_step5

---
+++ src/c_rank_level_up/c_rank_std_in_out_step5/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cout << "paiza";
+        if (i != (n - 1)) {
+            cout << " ";
+        } else {
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
+
