3bc7e89ceccd42c6d75be0e8b81f28ab32181a6c
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-07-09 23:45:01 +0900

    c_rank_level_up/c_rank_std_in_out_step6

---
+++ src/c_rank_level_up/c_rank_std_in_out_step6/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int tmp;
+        cin >> tmp;
+        cout << tmp << endl;
+    }
+
+    return 0;
+}
