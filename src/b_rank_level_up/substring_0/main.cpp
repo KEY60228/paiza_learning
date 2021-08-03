855cc70da8ab020ad3e103ea348c88a8ca98d072
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-08-03 23:08:41 +0900

    b_rank_level_up/substring_0

---
+++ src/b_rank_level_up/substring_0/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    cout << a << endl;
+    cout << b << endl;
+
+    return 0;
+}
