eed69c3fd73efb9eca2c6ca3036e6aca9dd6ec97
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-08-04 18:57:13 +0900

    b_rank_level_up/substring_upper_4

---
+++ src/b_rank_level_up/substring_upper_4/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    char c = toupper(s[0]);
+    cout << c << endl;
+
+    return 0;
+}
