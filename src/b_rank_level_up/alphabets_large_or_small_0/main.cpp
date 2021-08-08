becb3aa7e16bfb341fc828a365b7f781fa332f0d
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-08-08 19:32:53 +0900

    b_rank_level_up/alphabets_large_or_small_0

---
+++ src/b_rank_level_up/alphabets_large_or_small_0/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 3; i++) {
+        string s;
+        cin >> s;
+        cout << s << endl;
+    }
+
+    return 0;
+}
