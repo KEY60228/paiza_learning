fd9c5b5ca12a4ddb7d702d60b80801c4673765c7
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-08-20 20:27:34 +0900

    a_rank_level_up/snake_mapmove_step6

---
+++ src/a_rank_levele_up/snake_mapmove_step6/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int h, w, y, x, n;
+    cin >> h >> w >> y >> x >> n;
+
+    vector<string> v(h);
+    for (int i = 0; i < h; i++) {
+        cin >> v[i];
+    }
+
+    vector<int> vm(n);
+    vector<char> vd(n);
+    for (int i = 0; i < n; i++) {
+        cin >> vm[i];
+        cin >> vd[i];
+    }
+
+    int D = 100;
+    for (int i = 0; i < 100; i++) {
+        if (vm[0] == i) {
+            if (vd[0] == 'R') {
+                D++;
+            } else {
+                D--;
+            }
+            vm.erase(vm.begin(), vm.begin() + 1);
+            vd.erase(vd.begin(), vd.begin() + 1);
+        }
+        switch (D % 4) {
+            case 0:
+                y--;
+                break;
+            case 1:
+                x++;
+                break;
+            case 2:
+                y++;
+                break;
+            case 3:
+                x--;
+                break;
+        }
+        try {
+            if (v.at(y).at(x) == '#') {
+                throw exception();
+            }
+            cout << y << " " << x << endl;
+        } catch (exception const & e) {
+            cout << "Stop" << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
