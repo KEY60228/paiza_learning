c7372c095f108bc3a171b5ab6baa3e51017c6c3d
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-08-26 19:45:10 +0900

    a_rank_level_up/pincerattack_step7

---
+++ src/a_rank_levele_up/pincerattack_step7/main.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int h, w, n;
+    cin >> h >> w >> n;
+
+    vector<string> v(h);
+    for (int i = 0; i < h; i++) {
+        cin >> v[i];
+    }
+
+    for (int l = 0; l < n; l++) {
+        int y, x;
+        cin >> y >> x;
+        v[y][x] = '*';
+
+        // y方向
+        for (int i = -1; i <= 1; i++) {
+            int j = 1;
+            while (true) {
+                int tmpy = y + i * j;
+                if (tmpy < 0 || tmpy >= h) {
+                    break;
+                }
+                if (v[tmpy][x] == '#') {
+                    break;
+                }
+                if (v[tmpy][x] == '*') {
+                    for (int k = 1; k < j; k++) {
+                        v[y + i * k][x] = '*';
+                    }
+                    break;
+                }
+                j++;
+            }
+        }
+        // x方向
+        for (int i = -1; i <= 1; i++) {
+            int j = 1;
+            while (true) {
+                int tmpx = x + i * j;
+                if (tmpx < 0 || tmpx >= w) {
+                    break;
+                }
+                if (v[y][tmpx] == '#') {
+                    break;
+                }
+                if (v[y][tmpx] == '*') {
+                    for (int k = 1; k < j; k++) {
+                        v[y][x + i * k] = '*';
+                    }
+                    break;
+                }
+                j++;
+            }
+        }
+        // y-方向/x-方向, y+方向/x+方向
+        for (int i = -1; i <= 1; i++) {
+            int j = 1;
+            while (true) {
+                int tmpy = y + i * j;
+                int tmpx = x + i * j;
+                if (tmpy < 0 || tmpy >= h || tmpx < 0 || tmpx >= w) {
+                    break;
+                }
+                if (v[tmpy][tmpx] == '#') {
+                    break;
+                }
+                if (v[tmpy][tmpx] == '*') {
+                    for (int k = 1; k < j; k++) {
+                        v[y + i * k][x + i * k] = '*';
+                    }
+                    break;
+                }
+                j++;
+            }
+        }
+        // y-方向/x+方向, y+方向/x-方向
+        for (int i = -1; i <= 1; i++) {
+            int j = 1;
+            while (true) {
+                int tmpy = y + i * j;
+                int tmpx = x - i * j;
+                if (tmpy < 0 || tmpy >= h || tmpx < 0 || tmpx >= w) {
+                    break;
+                }
+                if (v[tmpy][tmpx] == '#') {
+                    break;
+                }
+                if (v[tmpy][tmpx] == '*') {
+                    for (int k = 1; k < j; k++) {
+                        v[y + i * k][x - i * k] = '*';
+                    }
+                    break;
+                }
+                j++;
+            }
+        }
+    }
+
+    for (int i = 0; i < h; i++) {
+        cout << v[i] << endl;
+    }
+
+    return 0;
+}
